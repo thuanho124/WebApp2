c527d4879377f7ec24d1be03a249935ffa92e516
Thuan Ho <thuanho241@gmail.com>
2019-08-03 17:26:16 -0700

    Library Management System

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementWithAuthen.Data;
+using LibraryManagementWithAuthen.Models;
+
+namespace LibraryManagementWithAuthen.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly LibDbContext _context;
+
+        public StudentsController(LibDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+
+            var student = await _context.Students
+                .Include(r => r.RentedBooks)
+                    .ThenInclude(b => b.Book)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.StudentID == id);
+
+            return View(student);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagementWithAuthen.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; } // primary key
+
+        [Required]
+        [StringLength(30)]
+        public string StudentFirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string StudentLastName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string StudentEmail { get; set; }
+
+
+        // navigation property
+        public ICollection<RentedBook> RentedBooks { get; set; }
+    }
+}
